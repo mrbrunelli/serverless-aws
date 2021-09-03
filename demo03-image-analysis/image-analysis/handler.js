e6a577da03e44bd628c14a8aad6dd7ec4e51fbc9
mrbrunelli <matheus.brunelli@gmail.com>
2021-09-02 21:19:25 -0300

    add aws image rekognition

---
+++ demo03-image-analysis/image-analysis/handler.js
@@ -0,0 +1,94 @@
+"use strict";
+
+const {
+  promises: { readFile },
+} = require("fs");
+
+class Handler {
+  constructor({ rekoSvc, translatorSvc }) {
+    this.rekoSvc = rekoSvc;
+    this.translatorSvc = translatorSvc;
+  }
+
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
+
+    const workingItems = result.Labels.filter(
+      ({ Confidence }) => Confidence > 80
+    );
+
+    const names = workingItems.map(({ Name }) => Name).join(" and");
+
+    return { names, workingItems };
+  }
+
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "pt",
+      Text: text,
+    };
+
+    const { TranslatedText } = await this.translatorSvc
+      .translateText(params)
+      .promise();
+    return TranslatedText.split(" e ");
+  }
+
+  async formatTextResults(texts, workingItems) {
+    const finalText = [];
+
+    for (const indexText in texts) {
+      const nameInPortuguese = texts[indexText];
+      const confidence = workingItems[indexText].Confidence;
+      finalText.push(
+        ` ${confidence.toFixed(2)}% de ser do tipo ${nameInPortuguese}`
+      );
+    }
+
+    return finalText.join("\n");
+  }
+
+  async main(event) {
+    try {
+      const imgBuffer = await readFile("./images/cat.jpg");
+      console.log("Detecting labels...");
+      const { names, workingItems } = await this.detectImageLabels(imgBuffer);
+
+      console.log("Translating to Portuguese...");
+      const texts = await this.translateText(names);
+
+      console.log("Handling final object...");
+      const finalText = await this.formatTextResults(texts, workingItems);
+      console.log("Finishing....");
+
+      return {
+        statusCode: 200,
+        body: "A imagem tem\n".concat(finalText),
+      };
+    } catch (error) {
+      console.log("Error***", error.stack);
+      return {
+        statusCode: 500,
+        body: "Internal server error",
+      };
+    }
+  }
+}
+
+// factory pattern, utilizar injeção de dependência para evitar adivinhações
+const aws = require("aws-sdk");
+const reko = new aws.Rekognition();
+const translator = new aws.Translate();
+const handler = new Handler({
+  rekoSvc: reko,
+  translatorSvc: translator,
+});
+
+module.exports.main = async (event) => handler.main(event);
